31bd138c84028fd1ec31328312daf87dc2db4bc3
IvanPiankov <vanypyankov@gmail.com>
2021-02-27 14:58:52 +0300

    DNAtoProtein

---
+++ DNAtoProtein.py
@@ -0,0 +1,45 @@
+from Bio import SeqIO
+
+def translate(seq):
+    table = {
+        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+        'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+        'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+    }
+    if len(seq) % 3 == 0:
+        protein=""
+        for i in range(0,len(seq),3):
+            codon=seq[i:i+3]
+            protein+=table[codon]
+    else:
+        protein = "No protein"
+    return protein
+
+
+def translater(n):
+    for _ in range(n):
+        next_elem = next(seq_record)
+        yield worker(next_elem)
+
+def worker(b):
+    id_seq = (f'its id of seq: {b.id}')
+    trans_seq = (translate(b.seq))
+    return id_seq, trans_seq
+
+
+seq_record = SeqIO.parse("sample.fasta", "fasta")
+
+print(list(translater(2)))
